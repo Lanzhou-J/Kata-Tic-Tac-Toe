dc9802d3dd72543c6867ce1d265c608aeba1ce9b
Lanzhou Jiang <lzjiang99@gmail.com>
2020-09-02 14:50:34 +1000

    add Piece class

--- Tic-Tac-Toe/Field.cs
+++ Tic-Tac-Toe/Field.cs
@@ -1,7 +1,27 @@
+using System;
+
 namespace Tic_Tac_Toe
 {
     public class Field
     {
-        
+        private Boolean HasPiece { get; set; }
+        private Coord Position { get; set; }
+        private Piece Piece { get; set; }
+
+        public Field()
+        {
+            HasPiece = false;
+        }
+
+        public Field AcceptMove()
+        {
+            this.HasPiece = true;
+            return this;
+        }
+
+        private Piece CreatePiece()
+        {
+            return new Piece();
+        }
     }
 }

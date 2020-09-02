3747428e5a07cd4830795b7d2c2a462aeeedfe94
Lanzhou Jiang <lzjiang99@gmail.com>
2020-09-02 18:31:24 +1000

    add Value attribute in Player class

--- Tic-Tac-Toe/Game.cs
+++ Tic-Tac-Toe/Game.cs
@@ -2,6 +2,10 @@ namespace Tic_Tac_Toe
 {
     public class Game
     {
-        
+        public static Player CurrentPlayer { get; set; }
+
+        public Game()
+        {
+        }
     }
 }
--- Tic-Tac-Toe/Player.cs
+++ Tic-Tac-Toe/Player.cs
@@ -2,6 +2,11 @@ namespace Tic_Tac_Toe
 {
     public class Player
     {
-        
+        public Piece.PieceValue Value { get; set; }
+
+        public Player()
+        {
+            
+        }
     }
 }

09fa67147dc6e06c59dc10805569ff98b9a230d1
Lanzhou Jiang <lzjiang99@gmail.com>
2020-09-07 17:49:21 +1000

    Add SortInput, QuitGame and CreateCoord functions in Game class

--- Tic-Tac-Toe/Game.cs
+++ Tic-Tac-Toe/Game.cs
@@ -1,9 +1,10 @@
 using System;
+using System.Text.RegularExpressions;
 namespace Tic_Tac_Toe
 {
     public class Game
     {
-        public Player CurrentPlayer { get; set; }
+        private Player CurrentPlayer { get; set; }
         public Player Player1 { get; set; }
         public Player Player2 { get; set; }
         public Board GameBoard { get; private set; }
@@ -22,9 +23,63 @@ namespace Tic_Tac_Toe
             Console.WriteLine("Here's the current board:");
             BoardOutput newBoardOutput = new BoardOutput(GameBoard);
             newBoardOutput.Print();
-            PlayerInput newInput = new PlayerInput();
-            string playerInput = newInput.CollectPlayerInput(CurrentPlayer.Name, CurrentPlayer.CellValue.ToString());
-            Console.WriteLine(playerInput);
+            int turn = 1;
+            while (turn < 9)  
+            {
+                if (turn % 2 == 0)
+                {
+                    CurrentPlayer = Player2;
+                }
+                else
+                {
+                    CurrentPlayer = Player1;
+                }
+
+                PlayerInput newInput = new PlayerInput();
+                string playerInput = newInput.CollectPlayerInput(CurrentPlayer.Name, CurrentPlayer.CellValue.ToString());
+                Board updatedBoard = SortInput(playerInput);
+                newBoardOutput = new BoardOutput(updatedBoard);
+                newBoardOutput.Print();
+                turn++;
+            }
+ 
+        }
+
+        private dynamic SortInput(string playerInput)
+        {
+            string pattern = @"^[1-3],[1-3]$";
+            if (playerInput == "q")
+            {
+                QuitGame(CurrentPlayer.Name);
+            }else if (Regex.IsMatch(playerInput, pattern))
+            {
+                Coord newCoord = CreateCoord(playerInput);
+                Board updatedBoard = GameBoard.UpdateBoard(newCoord, CurrentPlayer.CellValue);
+                return updatedBoard;
+            }
+            else
+            {
+                Console.WriteLine("It is not a valid input.");
+                return null;
+            }
+
+            return null;
+        }
+
+        private void QuitGame(string player)
+        {
+            Console.WriteLine($"{player} quit the game.");
+            Environment.Exit(1);
+        }
+
+        private Coord CreateCoord(string coordInput)
+        {
+            char xValue = coordInput[0];
+            char yValue = coordInput[2];
+            int coordX = (int) (xValue - '0');
+            int coordY = (int) (yValue - '0');
+            Coord newCoord = new Coord(coordX, coordY);
+            return newCoord;
         }
 
     }

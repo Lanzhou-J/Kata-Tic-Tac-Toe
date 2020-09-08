4bad39b6e67ddc0d7a07b696f99a67bf4318486b
Lanzhou Jiang <lzjiang99@gmail.com>
2020-09-08 14:01:32 +1000

    add test plan for JudgeTests

--- Tic_Tac_Toe_Tests/JudgeTests.cs
+++ Tic_Tac_Toe_Tests/JudgeTests.cs
@@ -2,6 +2,12 @@ namespace Tic_Tac_Toe_Tests
 {
     public class JudgeTests
     {
-        
+        //DetermineWinner() should return either a Player or null.
+        //When it is a draw, DetermineWinner() should return null.
+        //When 3 cells on the same row has same value -> a winner (player) is returned.
+        //When 3 cells on the same column has same value -> a winner (player) is returned.
+        //When 3 cells on the same diagonal has same value -> a winner (player) is returned.
+        //When Player 1 (X) wins, DetermineWinner should return Player 1.
+        //When Player 2 (O) wins, DetermineWinner should return Player 2. -> return correct winner.
     }
 }

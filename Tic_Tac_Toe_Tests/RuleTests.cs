57b013dffa206f03a3339157d4abc67aed9b2da3
Lanzhou Jiang <lzjiang99@gmail.com>
2020-09-15 17:14:41 +1000

    add test cases in RuleTests class to test DetermineWin function

--- Tic_Tac_Toe_Tests/RuleTests.cs
+++ Tic_Tac_Toe_Tests/RuleTests.cs
@@ -1,3 +1,6 @@
+using Tic_Tac_Toe;
+using Xunit;
+
 namespace Tic_Tac_Toe_Tests
 {
     public class RuleTests
@@ -7,5 +10,37 @@ namespace Tic_Tac_Toe_Tests
         //When 3 cells on the same row has same value -> return true.
         //When 3 cells on the same column has same value -> return true.
         //When 3 cells on the same diagonal has same value -> return true.
+
+        Board _newBoard = new Board(3);
+
+        [Fact]
+        public void DetermineWin_ShouldReturnTrue_When3SameValueCellsOnTheSameRow()
+        {
+            var board1 = _newBoard.UpdateBoard(new Coord(1, 1), CellValue.X);
+            var board2 = board1.UpdateBoard(new Coord(2, 1), CellValue.O);
+            var board3 = board2.UpdateBoard(new Coord(1, 2), CellValue.X);
+            var board4 = board3.UpdateBoard(new Coord(2, 2), CellValue.O);
+            var board5 = board4.UpdateBoard(new Coord(1, 3), CellValue.X);
+
+            var result = Rule.DetermineWin(board5, CellValue.X);
+            
+            Assert.True(result);
+        }
+
+        [Fact]
+        public void DetermineWin_ShouldReturnFalse_When2SameValueCellsOnTheSameRow()
+        {
+            var board1 = _newBoard.UpdateBoard(new Coord(1, 1), CellValue.X);
+            var board2 = board1.UpdateBoard(new Coord(2, 1), CellValue.O);
+            var board3 = board2.UpdateBoard(new Coord(1, 2), CellValue.X);
+            var board4 = board3.UpdateBoard(new Coord(2, 2), CellValue.O);
+            var board5 = board4.UpdateBoard(new Coord(1, 3), CellValue.X);
+        
+            var resultO = Rule.DetermineWin(board5, CellValue.O);
+            
+            Assert.False(resultO);
+        }
+
+
     }
 }

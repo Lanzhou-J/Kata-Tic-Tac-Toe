187ecd8bfd83105cb2eb672e01fb19a2a66b9bd0
Lanzhou Jiang <lzjiang99@gmail.com>
2020-09-15 16:33:27 +1000

    add test cases to test DisplayCellValue function for Cell class

--- Tic_Tac_Toe_Tests/CellTests.cs
+++ Tic_Tac_Toe_Tests/CellTests.cs
@@ -1,7 +1,36 @@
+using Tic_Tac_Toe;
+using Xunit;
+
 namespace Tic_Tac_Toe_Tests
 {
     public class CellTests
     {
+        // When Value is X, DisplayCellValue should return "X".
+        // When Value is O, DisplayCellValue should return "O".
+        // When Value is Empty, DisplayCellValue should return ".".
         
+        [Fact]
+        public void DisplayCellValue_WhenCellValueIsX_ShouldReturnStringX()
+        {
+            Coord newCoord = new Coord(1,1);
+            Cell newCell = new Cell(newCoord, CellValue.X);
+            Assert.Equal("X", newCell.DisplayCellValue());
+        }
+        
+        [Fact]
+        public void DisplayCellValue_WhenCellValueIsO_ShouldReturnStringO()
+        {
+            Coord newCoord = new Coord(1,1);
+            Cell newCell = new Cell(newCoord, CellValue.O);
+            Assert.Equal("O", newCell.DisplayCellValue());
+        }
+        
+        [Fact]
+        public void DisplayCellValue_WhenCellValueIsEmpty_ShouldReturnStringDot()
+        {
+            Coord newCoord = new Coord(1,1);
+            Cell newCell = new Cell(newCoord, CellValue.Empty);
+            Assert.Equal(".", newCell.DisplayCellValue());
+        }
     }
 }

337bc8cf6e86892e8e50dbaf6b3b776405240911
Lanzhou Jiang <lzjiang99@gmail.com>
2020-09-15 21:35:05 +1000

    add test cases in RuleTests class

--- Tic_Tac_Toe_Tests/RuleTests.cs
+++ Tic_Tac_Toe_Tests/RuleTests.cs
@@ -10,6 +10,8 @@ namespace Tic_Tac_Toe_Tests
         //When 3 cells on the same row has same value -> return true.
         //When 3 cells on the same column has same value -> return true.
         //When 3 cells on the same diagonal has same value -> return true.
+        //When only 2 same value cells on the same row -> return false.
+        //When 3 same value cells are not on the same line -> return false.
 
         Board _newBoard = new Board(3);
 
@@ -36,9 +38,79 @@ namespace Tic_Tac_Toe_Tests
             var board4 = board3.UpdateBoard(new Coord(2, 2), CellValue.O);
             var board5 = board4.UpdateBoard(new Coord(1, 3), CellValue.X);
         
-            var resultO = Rule.DetermineWin(board5, CellValue.O);
+            var result = Rule.DetermineWin(board5, CellValue.O);
             
-            Assert.False(resultO);
+            Assert.False(result);
+        }
+        
+        [Fact]
+        public void DetermineWin_ShouldReturnTrue_When3SameValueCellsOnTheSameColumn()
+        {
+            var board1 = _newBoard.UpdateBoard(new Coord(1, 3), CellValue.X);
+            var board2 = board1.UpdateBoard(new Coord(2, 1), CellValue.O);
+            var board3 = board2.UpdateBoard(new Coord(2, 3), CellValue.X);
+            var board4 = board3.UpdateBoard(new Coord(2, 2), CellValue.O);
+            var board5 = board4.UpdateBoard(new Coord(3, 3), CellValue.X);
+        
+            var result = Rule.DetermineWin(board5, CellValue.X);
+            
+            Assert.True(result);
+        }
+        
+        [Fact]
+        public void DetermineWin_ShouldReturnFalse_When2SameValueCellsOnTheSameColumn()
+        {
+            var board1 = _newBoard.UpdateBoard(new Coord(1, 3), CellValue.X);
+            var board2 = board1.UpdateBoard(new Coord(2, 1), CellValue.O);
+            var board3 = board2.UpdateBoard(new Coord(2, 2), CellValue.X);
+            var board4 = board3.UpdateBoard(new Coord(3, 1), CellValue.O);
+            var board5 = board4.UpdateBoard(new Coord(3, 2), CellValue.X);
+        
+            var result = Rule.DetermineWin(board5, CellValue.O);
+            
+            Assert.False(result);
+        }
+        
+        [Fact]
+        public void DetermineWin_ShouldReturnTrue_When3SameValueCellsOnTopLeftToBottomRightDiagonal()
+        {
+            var board1 = _newBoard.UpdateBoard(new Coord(1, 1), CellValue.X);
+            var board2 = board1.UpdateBoard(new Coord(2, 1), CellValue.O);
+            var board3 = board2.UpdateBoard(new Coord(2, 2), CellValue.X);
+            var board4 = board3.UpdateBoard(new Coord(3, 1), CellValue.O);
+            var board5 = board4.UpdateBoard(new Coord(3, 3), CellValue.X);
+        
+            var result = Rule.DetermineWin(board5, CellValue.X);
+            
+            Assert.True(result);
+        }
+        
+        [Fact]
+        public void DetermineWin_ShouldReturnTrue_When3SameValueCellsOnBottomLeftToTopRightDiagonal()
+        {
+            var board1 = _newBoard.UpdateBoard(new Coord(3, 1), CellValue.O);
+            var board2 = board1.UpdateBoard(new Coord(2, 1), CellValue.X);
+            var board3 = board2.UpdateBoard(new Coord(2, 2), CellValue.O);
+            var board4 = board3.UpdateBoard(new Coord(3, 2), CellValue.X);
+            var board5 = board4.UpdateBoard(new Coord(1, 3), CellValue.O);
+        
+            var result = Rule.DetermineWin(board5, CellValue.O);
+            
+            Assert.True(result);
+        }
+        
+        [Fact]
+        public void DetermineWin_ShouldReturnFalse_When3SameValueCellsAreNotOnTheSameLine()
+        {
+            var board1 = _newBoard.UpdateBoard(new Coord(1, 1), CellValue.O);
+            var board2 = board1.UpdateBoard(new Coord(2, 1), CellValue.X);
+            var board3 = board2.UpdateBoard(new Coord(2, 3), CellValue.O);
+            var board4 = board3.UpdateBoard(new Coord(3, 3), CellValue.X);
+            var board5 = board4.UpdateBoard(new Coord(3, 2), CellValue.O);
+        
+            var result = Rule.DetermineWin(board5, CellValue.O);
+            
+            Assert.False(result);
         }
 
 
